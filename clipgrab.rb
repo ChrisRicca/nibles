924cab9893aa61b23b8d4f42812a34946f98cb19
Chris Ricca <chris@dropio.com>
2009-09-17 10:53:00 -0400

    test dropio API commands

--- clipgrab.rb
+++ clipgrab.rb
@@ -1,9 +1,18 @@
 require 'rubygems'
 require 'sinatra'
-require 'erb'
 require 'dropio'
+require 'erb'
 require 'digest/sha1'
 
+configure :production do
+  Dropio.api_key = "3c9f26eb4cac5c03223d7f96b2286b68297c8686"
+end
+
+configure :development do
+  Dropio.api_key = "f4a700e04c1ce4d3d3087a3fdc576c176b6550a3"
+end
+
 get '/' do
-  "Hello World"
+  @drop = Dropio::Drop.create()
+  @drop.name
 end
