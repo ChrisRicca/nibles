3e8fbf6574b168f5c0ec6a45d8ae2d2a2324320e
Chris Ricca <chris@dropio.com>
2009-09-17 11:45:23 -0400

    creation of UI proof of concept

---
+++ javascripts/bookmarklet_concept.js
@@ -0,0 +1,4 @@
+UIhud = document.createElement('div');
+UIhud.setAttribute('style',"position:fixed;top:0px;right:0px;background-color:#EEEEEE;border-left:1px solid #333333;border-bottom:1px solid #333333;width:200px;height:40px;");
+
+document.body.appendChild(UIhud);
