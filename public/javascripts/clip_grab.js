27fe0568fad0c8ba8c04526e36acf75490b7927e
Chris Ricca <chris@dropio.com>
2009-09-18 04:32:52 -0400

    added corner links on images

--- public/javascripts/clip_grab.js
+++ public/javascripts/clip_grab.js
@@ -14,7 +14,7 @@ _ClipGrab.Grabber = function(){
   this.buildUI = function() {
     this.uiContainer = document.createElement('div');
     this.uiContainer.setAttribute('style',"position:fixed;top:0px;right:0px;background-color:#EEEEEE;border-left:1px solid #333333;border-bottom:1px solid #333333;width:200px;height:40px;z-index:9999;");
-    this.uiContainer.innerHTML = "<a href='http://drop.io/" + this.targetDrop().name + "'>View Your Nibbles</a>";
+    this.uiContainer.innerHTML = "<a href='http://drop.io/" + this.targetDrop().name + "'>View Your NIBLes</a>";
     document.body.appendChild(this.uiContainer);
   };
   
@@ -31,6 +31,7 @@ _ClipGrab.Grabber = function(){
   
   this.dropAction = function(action,message) {
     this.iFrame.contentWindow.location = this.iFrameURL + "#" + this.encodeMessage(action,message)
+    
   };
   
   this.encodeMessage = function(action,message) {
@@ -38,23 +39,74 @@ _ClipGrab.Grabber = function(){
   };
   
   this.activate = function() {
-    document.onmouseup = function(e) {
-      if (!e) var e = window.event;
-      var relTarg = e.relatedTarget || e.fromElement;
-      if (e.target.tagName == "IMG") {
-        
-        src = e.target.getAttribute('src');
-        if (src.indexOf('http') == -1) {
-          base = document.baseURI;
-          src = base.substring(0,base.lastIndexOf('/')) + src
-        }
-        _ClipGrab.grabber.addFile(src)
-      };
-      return false;
+    for each (var img in document.getElementsByTagName('img')) {
+      if(img.tagName == "IMG") _ClipGrab.buildLinkForImage(img);
     };
   };
   
   this.initialize();
 };
+
+_ClipGrab.buildLinkForImage = function(img) {
+  src = img.getAttribute('src');
+  if (src.indexOf('http') == -1) {
+    base = document.baseURI;
+    src = base.substring(0,base.lastIndexOf('/')) + src;
+  }
+  
+  img.style.border = "2px solid #000000"
+  a = document.createElement('a');
+  a.setAttribute('href',src);
+  a.innerHTML = 'NIBL this';
+  a.style.position = 'absolute';
+  a.style.left = _ClipGrab.findPosX(img) + img.offsetWidth + 'px';
+  a.style.top = _ClipGrab.findPosY(img) + img.offsetHeight + 'px';
+  a.style.zIndex = "99999";
+  a.style.backgroundColor = "#000000";
+  a.style.color = "#FFFFFF";
+  a.style.textDecoration = "none";
+  a.style.padding = "3px";
+  a.pairedClipGrabImage = img;
+
+  a.onclick = function() {
+    _ClipGrab.grabber.addFile(this.getAttribute('href'));
+    a.pairedClipGrabImage.style.border = "";
+    return false;
+  }
+  
+  document.body.appendChild(a);
+};
+
+_ClipGrab.findPosX = function(obj)
+{
+  var curleft = 0;
+  if(obj.offsetParent)
+      while(1) 
+      {
+        curleft += obj.offsetLeft;
+        if(!obj.offsetParent)
+          break;
+        obj = obj.offsetParent;
+      }
+  else if(obj.x)
+      curleft += obj.x;
+  return curleft;
+}
+
+_ClipGrab.findPosY = function(obj)
+{
+  var curtop = 0;
+  if(obj.offsetParent)
+      while(1)
+      {
+        curtop += obj.offsetTop;
+        if(!obj.offsetParent)
+          break;
+        obj = obj.offsetParent;
+      }
+  else if(obj.y)
+      curtop += obj.y;
+  return curtop;
+}
 
 _ClipGrab.grabber = new _ClipGrab.Grabber()
